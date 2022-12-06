85810f5a1fa116b6900ba75113ee0c6cdd187d46
nagyferenc <nagyferenc@EOTVOS2.LOCAL>
2022-12-06 10:55:58 +0100

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,55 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<form method="POST" action="{{ route('register') }}">
+    @csrf
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+    <!-- Name -->
+    <div>
+        <label for="name">Név:</label>
+        <input type="text" name="name" id="name" value="{{old('name')}}">
+        @error('name')
+            <div class="alert alert-danger">
+                {{$message}}
             </div>
+        @enderror
+    </div>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+    <!-- Email Address -->
+    <div class="mt-4">
+        <label for="email">E-mail:</label>
+        <input type="email" name="email" id="email" value="{{old('email')}}">
+        @error('email')
+            <div class="alert alert-danger">
+                {{$message}}
             </div>
+        @enderror
+    </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
+    <!-- Password -->
+    <div class="mt-4">
+        <label for="password">Jelszó:</label>
+        <input type="password" name="password" id="password">
+        @error('password')
+            <div class="alert alert-danger">
+                {{$message}}
             </div>
+        @enderror
+    </div>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
+    <!-- Confirm Password -->
+    <div class="mt-4">
+        <label for="password_confirmation">Jelszó újra:</label>
+        <input type="password" name="password_confirmation" id="password_confirmation">
+        @error('password_confirmation')
+            <div class="alert alert-danger">
+                {{$message}}
             </div>
+        @enderror
+    </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
+    <div class="">
+        <a class="" href="{{ route('login') }}">
+           Van már fiókod? Jelentkezz be itt!
+        </a>
 
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+        <button type="submit">Regisztráció</button>
+    </div>
+</form>
